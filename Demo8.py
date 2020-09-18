8d517a3f70d0cac581373ba08fdfc2665a076773
yws191 <54100426+yws191@users.noreply.github.com>
2020-09-18 21:36:16 +0800

    Add files via upload

---
+++ Demo8.py
@@ -0,0 +1,12 @@
+#逻辑运算符
+age = 25
+gender = '男'
+print(age > 20 and gender == '男')
+sno = 30
+age = 18
+print(sno > 20 or age >30)
+age = 28
+gender = '女'
+print(age > 20 and gender == '男')
+a = True
+print(not a)
