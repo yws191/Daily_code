a9c0c0126ef67aaeea1a0fa7ecc2f2bd6543f3e2
yws191 <54100426+yws191@users.noreply.github.com>
2020-09-26 21:53:17 +0800

    Add files via upload

---
+++ Demo3.py
@@ -0,0 +1,25 @@
+#数据类型
+n1 = 20  #整型
+print(n1)
+n2 = 4.55 #浮点型
+print(n2)
+n1 = 40
+print(n1)
+n1 = 9.89
+print(n1)
+s1 = "hello" #字符串
+print(s1)
+s2 = 'haha'
+print(s2)
+s2 = 100
+print(s2)
+s2 = 9.8988
+print(s2)
+n1 = "大家好"
+print(n1)
+n2 = "heihei"
+print(n2)
+b1 = True #布尔类型
+print(b1)
+b2 = False
+print(b2)
