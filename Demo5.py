9c966fc0d9d96cce44eec254f23d4c7d461c5106
yws191 <54100426+yws191@users.noreply.github.com>
2020-09-30 19:08:57 +0800

    Add files via upload

---
+++ Demo5.py
@@ -0,0 +1,32 @@
+a = 100
+print(type(a) , a)
+#a转换为字符串
+a = str(a)
+print(type(a) , a)
+a = float(a)  #a转换为浮点型
+print(type(a) , a)
+a = bool(a)   #a转换为布尔型
+print(type(a) , a)
+b = 5.77
+b = bool(b)  #b转换为布尔型
+print(type(b) , b)
+c = 0
+c = bool(c)  #c转换为布尔型
+print(type(c) , c)
+s = '12389'
+s = int(s)  #s转换为整型，字符串必须由数字组成才能转换
+print(type(s) , s)
+s = bool(s)
+print(type(s) , s)
+d = 9.87
+d = int(d)  #浮点转整数
+print(type(d) , d)
+e = True
+e = int(e)
+print(type(e) , e)
+f = False
+f = int(f)
+print(type(f) , f)
+g = False
+g = str(g)
+print(type(g) , g)
