7135b6191b7b32eda522af2f295335cda8070e78
yws191 <54100426+yws191@users.noreply.github.com>
2020-10-08 10:05:50 +0800

    Add files via upload

---
+++ Demo6.py
@@ -0,0 +1,15 @@
+#算术运算符
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(b / a)
+print(b % a)
+c = 10
+d = 3
+print(c / d)
+print(a + b * c)
+print((a + b) / (c - d))
